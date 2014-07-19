fbdc67e3b7ea303f2bf79568d798a6a8363b13d0
Jimbow <adeathjohn@gmail.com>
2014-07-19 08:31:27 +0800

    sdfs

---
+++ UiNavigation/TrnthUiNavigation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TrnthUiNavigation : TrnthPoolBase {
+	static public TrnthUiNavigation main;
+	public List<GameObject> list;
+	public void push(GameObject prefab){
+		GameObject e=Spawn(prefab);
+		list.Add(e);
+		e.transform.position=pos;		
+	}
+	public void pop(){
+		if(list.Count<1)return;
+		var last=list[list.Count-1];
+		list.Remove(last);
+		Despawn(last);
+	}
+	public override void Awake(){
+		base.Awake();
+		main=this;
+	}
+}
