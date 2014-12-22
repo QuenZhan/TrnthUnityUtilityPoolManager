15401a97a6eee6fd166c91b994b87b8fb7f1f400
JimbowMBPR <adeathjohn@gmail.com>
2014-12-23 04:12:14 +0800

    no message

---
+++ TrnthSpawnBoucingNumber.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using TRNTH;
+public class TrnthSpawnBoucingNumber:TrnthSpawn{
+	public int damage;
+	public override GameObject execute(){
+		var instance=base.execute();
+		if(instance){
+			var bn=instance.GetComponent<TrnthBoucingNumber>();
+			bn.setup((int)damage);
+		}
+		return instance;
+	}
+}
