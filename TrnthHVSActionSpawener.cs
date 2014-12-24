185719589a0516cdc303a647d3cbd997504da30a
JimbowMBPR <adeathjohn@gmail.com>
2014-12-24 17:01:10 +0800

    asdf

---
+++ TrnthHVSActionSpawener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using TRNTH;
+
+[RequireComponent(typeof(TrnthSpawn))]
+public class TrnthHVSActionSpawener : TrnthHVSAction{
+	protected override void _execute(){
+		if(!_spawner)_spawner=GetComponent<TrnthSpawn>();
+		_spawner.execute();
+	}
+	TrnthSpawn _spawner;
+}
