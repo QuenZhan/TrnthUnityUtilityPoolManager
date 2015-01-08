5ef14707066a70e01384604124450d32a56ba276
JimbowMBPR <adeathjohn@gmail.com>
2015-01-09 04:15:34 +0800

    fdsfdf

---
+++ TrnthHVSActionDespawn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+public class TrnthHVSActionDespawn : TrnthHVSActionPoolBase {
+	protected override void _execute(){
+		var tra=prefab.transform;
+		// if(!tra.gameObject.activeInHierarchy)return;
+		var pool=PoolManager.Pools[this.pool];
+		if(pool.IsSpawned(tra))pool.Despawn(tra);
+	}
+}
---
+++ TrnthHVSActionSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+public class TrnthHVSActionSpawn : TrnthHVSActionPoolBase {
+	public bool beChild=false;
+	public bool positionFit=true;
+	public bool rotationFit;
+	public Transform spawned{get;private set;}
+	protected override void _execute(){
+		if(!PoolManager.Pools.ContainsKey(this.pool))return;
+		// var pool=PoolManager.Pools[this.pool];
+		base._execute();
+		spawned=PoolManager.Pools[this.pool].Spawn(prefab.transform);
+		if(positionFit)spawned.position=transform.position;
+		if(rotationFit)spawned.eulerAngles=transform.eulerAngles;
+		if(beChild)spawned.parent=transform;
+	}
+}
