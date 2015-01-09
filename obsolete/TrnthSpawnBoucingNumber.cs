eaedbd26828eab3c0126e6868e26ab488d3d1ce0
JimbowMBPR <adeathjohn@gmail.com>
2015-01-10 05:04:28 +0800

    sdfs

--- obsolete/TrnthSpawnBoucingNumber.cs
+++ obsolete/TrnthSpawnBoucingNumber.cs
@@ -3,8 +3,15 @@ using System.Collections;
 using System.Linq;
 using TRNTH;
 public class TrnthSpawnBoucingNumber:TrnthSpawn{
+	public TrnthAttackReceiver reciever;
+	// public 
+	// public TrnthSender
 	public int damage;
 	public override GameObject execute(){
+		if(reciever){
+			if(!reciever.attack.showDamage)return null;
+			damage=(int)reciever.damage;
+		}
 		var instance=base.execute();
 		if(instance){
 			var bn=instance.GetComponent<TrnthBoucingNumber>();
