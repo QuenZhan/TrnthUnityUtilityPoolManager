06d50b06a6d76eafbd3cc622068e27b800651c8b
JimbowMBPR <adeathjohn@icloud.com>
2015-05-09 12:38:33 +0800

    projectile

---
+++ TrnthHVSActionSpawnProjectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionSpawnProjectile : TrnthHVSActionSpawn {
+	public Vector3 velocityInit;
+	public Vector3 noise;
+	protected override void _execute(){
+		base._execute();
+		var instance=spawned;
+		var rig=instance.GetComponent<TrnthTemplateProjectile>().rigid;
+		var vec=new Vector3(velocityInit.x+Random.value*noise.x
+			,velocityInit.y+Random.value*noise.y
+			,velocityInit.z+Random.value*noise.z);
+		rig.velocity=transform.TransformDirection(vec);
+	}
+}
---
+++ TrnthTemplateProjectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthTemplateProjectile : MonoBehaviour {
+	public float lifetime=5;
+	public float timerActivate=0.5f;
+	public Collider col;
+	public Rigidbody rigid;
+	public ParticleSystem loop;
+	public TrnthHVSActionSpawn fxStart;
+	public TrnthHVSActionSpawn fxHit;
+	public TrnthHVSActionSpawn fxEnd;
+	public TrnthHVSCondition attackSender;
+	public void initialize(){
+		col.enabled=false;
+		rigid.transform.localPosition=Vector3.zero;
+		loop.Play();
+		fxStart.execute();
+		Invoke("enableCollider",timerActivate);
+		Invoke("hit",5);
+	}
+	public void hit(){
+		col.enabled=false;
+		loop.Stop();
+		fxHit.execute();
+	}
+	void OnDespawn(){
+		rigid.transform.localPosition=Vector3.zero;
+		col.enabled=false;
+	}
+	void enableCollider(){
+		col.enabled=true;
+	}
+}
